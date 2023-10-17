99feff4ee99d7760d49579cd428838062133b8a4
xxkinaxx <ekopandu088@gmail.com>
2023-10-17 08:04:37 +0700

    changes

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,9 +1,9 @@
-const menuButton = document.getElementsByClassName('menu')[0];
-const navbarItem =document.getElementsByClassName('navbar-item');
-menuButton.addEventListener('click', function(){
-    for(let i = 0; i < navbarItem.length; i++)
-        navbarItem[i].classList.toggle('active')
-});
+// const menuButton = document.getElementsByClassName('menu')[0];
+// const navbarItem =document.getElementsByClassName('navbar-item');
+// menuButton.addEventListener('click', function(){
+//     for(let i = 0; i < navbarItem.length; i++)
+//         navbarItem[i].classList.toggle('active')
+// });
 
 // script header
 let nav = document.querySelector('nav');
